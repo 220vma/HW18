6874c2a15d26e110238a06e6155b134595050f15
220vma <59931588+220vma@users.noreply.github.com>
2020-05-07 14:35:00 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,16 @@
+import os
+
+def find_files(dir):
+
+    for i in dir_list(dir):
+        if os.path.isdir(i):
+            find_files(i)
+        else:
+            print(i)
+
+def dir_list(dir):
+    for name in os.listdir(dir):
+        path = os.path.join(dir, name)
+        yield path
+
+find_files("C:\MSI")
